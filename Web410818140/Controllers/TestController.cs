58a36c8b380c0bded54dd0eceaf65777cd43b30a
劉諺鴻 <davidalan0826@gmail.com>
2021-05-27 20:09:23 +0800

    hw3

--- Web410818140/Controllers/TestController.cs
+++ Web410818140/Controllers/TestController.cs
@@ -22,5 +22,10 @@ namespace Web410818140.Controllers
         {
             return View();
         }
+
+        public ActionResult HW3()
+        {
+            return View();
+        }
     }
 }
